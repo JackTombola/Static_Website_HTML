f0e1579d2811b72a0e647e838949121850ba824a
Jack <jack.levitt@tombola.com>
2019-09-06 14:36:16 +0100

    Sign Up Page

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
